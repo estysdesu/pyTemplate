4270c8521d21b1601b976f5b912f3cf742fde4f2
Tyler Estes <37026871+estysdesu@users.noreply.github.com>
2019-06-24 15:16:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-import sys
+import sys, logging
 from logger import Log
 
 def main():
-    Log.info("app starting")
+    log.info("app starting")
     return
     
 if __name__ == "__main__":
+    log = logging.getLogger("root")
+    log.setLevel(logging.DEBUG)
+    sh = logHandlers.StreamHandler()
+    log.addHandler(sh)
+    logDir = os.path.join( os.getcwd(), "<.appName>")
+    logFile = os.path.join(logDir, "root.log")
+    os.makedirs(logDir, exist_ok=True)
+    fh = logHandlers.FileHandler(logFile)
+    log.addHandler(fh)
+   
     sys.exit(main())
