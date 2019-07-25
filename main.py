2ec22c9355e8e2198dff241e4884cb507092fe3b
Tyler Estes <37026871+estysdesu@users.noreply.github.com>
2019-07-25 10:50:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import os
+import logging
 import sys
 
 import log_handlers
@@ -12,6 +13,8 @@ def main():
 
 
 if __name__ == "__main__":
-    log_dir = os.getcwd()
-    log = log_handlers.logger_setup(name="root", log_dir=log_dir)
+    log_dir = os.path.join(os.getcwd(), ".log")
+    log = log_handlers.logger_setup(
+        name="root", log_file_dir=log_dir, log_file_level=logging.DEBUG)
+    )
     sys.exit(main())
