3d411737e5accd84fd4419a27881cfeb20342036
Tyler Estes <37026871+estysdesu@users.noreply.github.com>
2019-07-25 09:11:12 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ def main():
 
 if __name__ == "__main__":
     log_dir = os.getcwd()
-    log = log_handlers.logger_setup(log_dir)
+    log = log_handlers.logger_setup(name="root", log_dir=log_dir)
     sys.exit(main())
